37f4327e15ab055f5783c158363c589202d15ee7
LifeCANvs <162405867+LifeCANvs@users.noreply.github.com>
2024-11-06 16:45:39 +0700

    Update test_xmppaddr.c

--- tests/test_xmppaddr.c
+++ tests/test_xmppaddr.c
@@ -1,14 +1,15 @@
-/* SPDX-License-Identifier: MIT OR GPL-3.0-only */
+/* SPDX-License-Identifier: MIT */
 /* test_xmppaddr.c
 ** libstrophe XMPP client library -- test routines for the xmppaddr
 ** certificate API's
 **
 ** Copyright (C) 2021 Steffen Jaeckel
+** Copyright 2024 LifeCAN!vs
 **
-**  This software is provided AS-IS with no warranty, either express
-**  or implied.
+**  This software is provided AS-IS with no warranty, either express or
+**  implied.
 **
-**  This program is dual licensed under the MIT or GPLv3 licenses.
+** This program is licensed under the MIT license.
 */
 
 #include <assert.h>
