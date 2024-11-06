7b376329a4a1bfbf92971002814d5b357ce4685a
LifeCANvs <162405867+LifeCANvs@users.noreply.github.com>
2024-11-06 16:49:54 +0700

    Update roster.c

--- examples/roster.c
+++ examples/roster.c
@@ -1,13 +1,14 @@
-/* SPDX-License-Identifier: MIT OR GPL-3.0-only */
+/* SPDX-License-Identifier: MIT */
 /* roster.c
 ** libstrophe XMPP client library -- handler example
 **
 ** Copyright (C) 2005-2009 Collecta, Inc.
+** Copyright 2024 LifeCAN!vs
 **
-**  This software is provided AS-IS with no warranty, either express
-**  or implied.
+**  This software is provided AS-IS with no warranty, either express or
+**  implied.
 **
-** This program is dual licensed under the MIT or GPLv3 licenses.
+** This program is licensed under the MIT license.
 */
 
 /* This example demonstrates basic handler functions by printing out
