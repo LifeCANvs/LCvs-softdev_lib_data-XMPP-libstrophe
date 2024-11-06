98c4f4350353d24d91157cc257c9e76ada16c3ed
LifeCANvs <162405867+LifeCANvs@users.noreply.github.com>
2024-11-06 16:37:43 +0700

    Update strophe.h

--- strophe.h
+++ strophe.h
@@ -1,13 +1,14 @@
-/* SPDX-License-Identifier: MIT OR GPL-3.0-only */
+/* SPDX-License-Identifier: MIT */
 /* strophe.h
 ** strophe XMPP client library C API
 **
 ** Copyright (C) 2005-2009 Collecta, Inc.
+** Copyright 2024 LifeCAN!vs
 **
 **  This software is provided AS-IS with no warranty, either express or
 **  implied.
 **
-**  This software is dual licensed under the MIT or GPLv3 licenses.
+** This program is licensed under the MIT license.
 */
 
 /** @file
